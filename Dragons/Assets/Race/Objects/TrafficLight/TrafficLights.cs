bf0e0023c4b99b5932af471ec31632a908f35323
TheDizzler <t.gillon@dico4u.com>
2020-02-19 19:25:19 +0900

    traffic lights and traffic cones ohmagerd

---
+++ Dragons/Assets/Race/Objects/TrafficLight/TrafficLights.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class TrafficLights : MonoBehaviour
+{
+
+	[SerializeField] private Light[] redLights = new Light[2];
+	[SerializeField] private Light[] yellowLights = new Light[2];
+	[SerializeField] private Light[] greenLights = new Light[2];
+
+	[SerializeField] private Material redLightMat = null;
+	[SerializeField] private Material yellowLightMat = null;
+	[SerializeField] private Material greenLightMat = null;
+
+	public void RedLight()
+	{
+		foreach (Light light in redLights)
+			light.enabled = true;
+		foreach (Light light in yellowLights)
+			light.enabled = false;
+		foreach (Light light in greenLights)
+			light.enabled = false;
+		redLightMat.EnableKeyword("_EMISSION");
+		yellowLightMat.DisableKeyword("_EMISSION");
+		greenLightMat.DisableKeyword("_EMISSION");
+	}
+
+	public void YellowLight()
+	{
+		foreach (Light light in redLights)
+			light.enabled = false;
+		foreach (Light light in yellowLights)
+			light.enabled = true;
+		foreach (Light light in greenLights)
+			light.enabled = false;
+		redLightMat.DisableKeyword("_EMISSION");
+		yellowLightMat.EnableKeyword("_EMISSION");
+		greenLightMat.DisableKeyword("_EMISSION");
+	}
+
+	public void GreenLight()
+	{
+		foreach (Light light in redLights)
+			light.enabled = false;
+		foreach (Light light in yellowLights)
+			light.enabled = false;
+		foreach (Light light in greenLights)
+			light.enabled = true;
+		redLightMat.DisableKeyword("_EMISSION");
+		yellowLightMat.DisableKeyword("_EMISSION");
+		greenLightMat.EnableKeyword("_EMISSION");
+	}
+
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Z))
+			RedLight();
+		if (Input.GetKeyDown(KeyCode.X))
+			YellowLight();
+		if (Input.GetKeyDown(KeyCode.C))
+			GreenLight();
+	}
+}
---
+++ Dragons/Assets/Race/Racers/Indicator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Indicator : MonoBehaviour
+{
+	[SerializeField] private bool up = true;
+	[SerializeField] private float cycleDistance = 0;
+	[SerializeField] private float cycleTime = 0;
+	private Vector3 centerPos;
+	private Vector3 topPos;
+	private Vector3 bottomPos;
+	private float totalTime = 0;
+
+
+	public void Start()
+	{
+		centerPos = transform.localPosition;
+		topPos = centerPos;
+		topPos.y += cycleDistance;
+		bottomPos = centerPos;
+		bottomPos.y -= cycleDistance;
+	}
+
+	public void Update()
+	{
+		if (up)
+			totalTime += Time.deltaTime;
+		else
+			totalTime -= Time.deltaTime;
+
+		float t = Mathf.Sign(totalTime) * totalTime * totalTime / cycleTime;
+		if (t > 1)
+			up = false;
+		if (t < 0)
+			up = true;
+		transform.localPosition = Vector3.Lerp(topPos, bottomPos,  t);
+	}
+}
