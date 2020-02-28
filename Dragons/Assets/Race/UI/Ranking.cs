c1cd6312ed2279d6d12603d99434b3120a0778d4
TheDizzler <t.gillon@dico4u.com>
2020-02-28 19:58:13 +0900

    untested pizzazz

--- Dragons/Assets/Race/UI/Ranking.cs
+++ Dragons/Assets/Race/UI/Ranking.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -6,28 +7,64 @@ namespace AtomosZ.Gambale.Keiba
 {
 	public class Ranking : MonoBehaviour
 	{
-		[SerializeField] private TextMeshProUGUI firstPlace = null;
-		[SerializeField] private TextMeshProUGUI secondPlace = null;
-		[SerializeField] private TextMeshProUGUI thirdPlace = null;
+		[SerializeField] private TextMeshProUGUI[] rankingLabels = new TextMeshProUGUI[3];
+		[SerializeField] private WagerManager wagerManager = null;
 
 		private List<Horse> ranking = new List<Horse>();
+		private Color color1 = Color.red;
+		private Color color2 = new Color(.5f, 0, 1, 1);
+		private Color color3 = Color.cyan;
+		private Color color4 = new Color(.5f, 1, 0, 1);
 
 
 		public void RacerFinished(Horse horse)
 		{
+			this.gameObject.SetActive(true);
 			ranking.Add(horse);
 			switch (ranking.Count)
 			{
 				case 1:
 					gameObject.SetActive(true);
-					firstPlace.text = horse.name;
+					rankingLabels[0].text = horse.name;
 					break;
 				case 2:
-					secondPlace.text = horse.name;
+					rankingLabels[1].text = horse.name;
 					break;
 				case 3:
-					thirdPlace.text = horse.name;
+					rankingLabels[2].text = horse.name;
+					foreach (int winner in wagerManager.Payout(ranking))
+					{
+						StartCoroutine(ColorPizzazz(rankingLabels[winner - 1]));
+					}
 					break;
+			}
+		}
+
+
+		private IEnumerator ColorPizzazz(TextMeshProUGUI placingText)
+		{
+			VertexGradient magicalColors = new VertexGradient(color1, color2, color3, color4);
+			placingText.enableVertexGradient = true;
+			placingText.colorGradient = magicalColors;
+
+			while (true)
+			{
+				yield return null;
+				float t = Time.deltaTime;
+				Color c1 = Color.Lerp(color1, color2, t);
+				Color c2 = Color.Lerp(color2, color3, t);
+				Color c3 = Color.Lerp(color3, color4, t);
+				Color c4 = Color.Lerp(color4, color1, t);
+				magicalColors.topLeft = c1;
+				magicalColors.topRight = c2;
+				magicalColors.bottomRight = c3;
+				magicalColors.bottomLeft = c4;
+				color1 = c1;
+				color2 = c2;
+				color3 = c3;
+				color4 = c4;
+				placingText.colorGradient = magicalColors;
+
 			}
 		}
 	}
--- Dragons/Assets/Race/UI/WagerManager.cs
+++ Dragons/Assets/Race/UI/WagerManager.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using AtomosZ.UI;
+using TMPro;
 using UnityEngine;
 using static AtomosZ.Gambale.Keiba.Wagers;
 
@@ -6,9 +8,16 @@ namespace AtomosZ.Gambale.Keiba
 {
 	public class WagerManager : MonoBehaviour
 	{
-		[SerializeField] private Wagers wagerType = null;
+		[SerializeField] private Wagers wagers = null;
+		[SerializeField] private Spinner spinner = null;
 		[SerializeField] private RacerSelectDisplay rsd = null;
+		[SerializeField] private TextMeshProUGUI payout = null;
 		[SerializeField] private RaceManager raceManager = null;
+
+		private WagerType wagerType;
+		private List<Horse> picks;
+		private int amountWagered;
+
 
 		public void Start()
 		{
@@ -17,11 +26,55 @@ namespace AtomosZ.Gambale.Keiba
 
 		public void PlaceBet()
 		{
-			WagerType type = wagerType.GetWagerType();
-			List<Horse> picks = rsd.GetSelectedRacers();
-			int amount = 100; // temp
+			wagerType = wagers.GetWagerType();
+			picks = rsd.GetSelectedRacers();
+			amountWagered = spinner.currentValue;
 			raceManager.StartRace();
 			this.gameObject.SetActive(false);
+		}
+
+		public List<int> Payout(List<Horse> ranking)
+		{
+			List<int> winners = new List<int>();
+			int payoutAmount = 0;
+			
+			switch (wagerType)
+			{
+				case WagerType.Win:
+					if (picks[0] == ranking[0])
+					{
+						Debug.Log("the winningest");
+						payoutAmount += 1000;
+						winners.Add(0);
+					}
+					break;
+				case WagerType.Place:
+					for (int i = 0; i < ranking.Count; ++i)
+					{
+						if (picks[0] == ranking[i])
+						{
+							Debug.Log("Place!");
+							payoutAmount += 500;
+							winners.Add(i);
+						}
+					}
+					break;
+				case WagerType.Show:
+					for (int i = 0; i < ranking.Count; ++i)
+					{
+						if (picks[0] == ranking[i])
+						{
+							Debug.Log("Show!");
+							payoutAmount += 20;
+							winners.Add(i);
+						}
+					}
+					break;
+			}
+
+			payout.gameObject.SetActive(true);
+			payout.text = "$" + payoutAmount;
+			return winners;
 		}
 	}
 }
